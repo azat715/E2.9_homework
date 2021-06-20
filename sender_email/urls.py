1bf2b6f7d1180d828095714f60196590a7b7236d
azat715 <azat715@gmail.com>
2021-06-20 19:26:40 +0500

    настройки и urls front app

--- sender_email/urls.py
+++ sender_email/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from front.views import main
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("core.urls", namespace="core")),
+    path("", main, name="main"),
 ]
